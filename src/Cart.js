f92e7d1bda05bcd392fdc0a36ef9c6859f48c418
royneau <royneau@freshtouch.fr>
2019-06-22 16:51:02 +0200

    feat: display total price and best offer

--- src/Cart.js
+++ src/Cart.js
@@ -1,19 +1,73 @@
-import React from 'react'
+import React, { Component } from 'react'
 import CartItem from './CartItem';
 
-function Cart({ cart }) {
-  return (
-    <div className="Cart">
-      <h1>Mon panier</h1>
-      <ul>
-        {Object.keys(cart).map(key =>
-          <li key={key}>
-            <CartItem item={cart[key]} />
-          </li>
-        )}
-      </ul>
-    </div>
-  );
+class Cart extends Component {
+
+  state = {
+    totalPrice: 0,
+    bestOffer: 0,
+  }
+
+  componentDidMount() {
+    this.calculatePrice(this.props.cart)
+  }
+
+  componentWillReceiveProps({ cart }) {
+    this.calculatePrice(cart)
+  }
+
+  calculatePrice(cart) {
+    /* calculate the total price */
+    var totalPrice = 0
+    Object.keys(cart).map(key => {
+      const item = cart[key]
+      totalPrice += (item.amount * item.price)
+    })
+    this.setState({ totalPrice })
+
+    /* calculate the best offer */
+    fetch(`http://henri-potier.xebia.fr/books/${Object.keys(cart).join()}/commercialOffers`)
+      .then((response) => {
+        return response.json()
+      })
+      .then((myJson) => {
+        const bestOffer = this.calculatebestOffer(totalPrice, myJson.offers)
+        this.setState({ bestOffer })
+      })
+  }
+
+  calculatebestOffer(price, offers) {
+    const proposedOffers = []
+    offers.forEach(offer => {
+      if (offer.type === 'percentage') {
+        proposedOffers.push(price - (price * offer.value / 100))
+      } else if (offer.type === 'minus') {
+        proposedOffers.push(price - offer.value)
+      }
+      else if (offer.type === 'slice') {
+        const slices = Math.floor(price / offer.sliceValue)
+        proposedOffers.push(price - slices * offer.value)
+      }
+    })
+    return Math.min(...proposedOffers)
+  }
+
+  render() {
+    return (
+      <div className="Cart">
+        <h1>Mon panier</h1>
+        <ul>
+          {Object.keys(this.props.cart).map(key =>
+            <li key={key}>
+              <CartItem item={this.props.cart[key]} />
+            </li>
+          )}
+        </ul>
+        <div>Total : {this.state.totalPrice}</div>
+        <div>Offre commerciale : {this.state.bestOffer}</div>
+      </div>
+    )
+  }
 }
 
 export default Cart
